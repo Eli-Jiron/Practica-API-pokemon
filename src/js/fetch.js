42c7c9ed1c80ad26741f1b07797ece46eed4a7ef
Eli-Jiron <elianguevara28@gmail.com>
2024-06-11 12:57:22 -0600

    Refactorizacion

---
+++ src/js/fetch.js
@@ -0,0 +1,9 @@
+export const get = async (id) => {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,29 @@
-let captura = document.getElementById('captura');
-let capturado = document.getElementById('capturado');
+import { get } from "./fetch.js";
+
+let capturado = document.getElementById("capturado");
 
 class pokedex {
-    constructor (id, nombre, tipo) {
-        this.id = id;
-        this.nombre = nombre;
-        this.tipo = tipo;
+  constructor(id, nombre, tipo) {
+    this.id = id;
+    this.nombre = nombre;
+    this.tipo = tipo;
+  }
+  mensaje() {
+    console.log(this.id, this.nombre, this.tipo);
+    if (this.tipo.length === 1) {
+      capturado.textContent = `¡Capturaste un ${this.nombre}! Tipo: ${this.tipo[0].type.name}`;
+    } else {
+      capturado.textContent = `¡Capturaste un ${this.nombre}! Tipo: ${this.tipo[0].type.name}, ${this.tipo[1].type.name}`;
     }
-    mensaje () {
-        console.log(this.id, this.nombre, this.tipo);
-        if (this.tipo.length === 1) {
-            capturado.textContent = `¡Capturaste un ${this.nombre}! Tipo: ${this.tipo[0].type.name}`;
-        } else {
-            capturado.textContent = `¡Capturaste un ${this.nombre}! Tipo: ${this.tipo[0].type.name}, ${this.tipo[1].type.name}`;
-        }
-    }
+  }
 }
 
-async function capturarPokemon(id) {
-    try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-        const data = await response.json();
-        let a = new pokedex(data.id, data.name, (data.type, data.types));
-        a.mensaje();
-        return data;
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-captura.addEventListener('click', function () {
-    let id = Math.floor(Math.random() * 1026);
-    console.log(capturarPokemon(id));
-})
+document.getElementById("btnCaptura").addEventListener("click", async () => {
+  const promise = await get(Math.floor(Math.random() * 1026));
+  if (promise !== undefined) {
+    let captura = new pokedex(promise.id, promise.name, (promise.type, promise.types));
+    captura.mensaje();
+  } else {
+    alert("Ha ocurrido un error de servidor");
+  } 
+});
